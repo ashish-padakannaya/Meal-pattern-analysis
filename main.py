e058e905e0046a2167aa83153aec5c9035d7f153
Yash Vijay <yashvijay_17@yahoo.co.in>
2019-09-29 21:31:27 -0700

    Added RMS and SD

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import pandas as pd
 from configparser import ConfigParser
 import ast
 from helper import convert_to_epoch, getFloatFromObject
-from features import get_fft
+from features import get_fft, get_sd, get_rms
 import os
 import numpy as np
+from tsfresh.feature_extraction.feature_calculators import standard_deviation
 
 def getDataFrame():
     #read config file and get patient data sources
@@ -22,7 +23,6 @@ def getDataFrame():
         cgm_frame = pd.read_csv(data_path, na_filter=False)
         time_frame_array = time_frame.to_numpy()
         cgm_frame_array = cgm_frame.to_numpy()
-        print(time_frame_array.shape)
         patientMealCountMap[patient_number] = time_frame_array.shape[0]
 
         #zip functions joins each ith element of 2 arrays together:
@@ -46,5 +46,13 @@ def getDataFrame():
 
     return patient_df
 
-test = getDataFrame()
-get_fft(test).to_csv('fft.csv', index=False)
+patient_df = getDataFrame()
+get_fft(patient_df).to_csv('fft.csv', index=False)
+
+# Code to retrieve STANDARD DEVIATION (SD) per person per meal
+# Check patient_df_df for SD Dataframe
+patient_sd_df = get_sd(patient_df)
+
+# Code to retrieve ROOT MEAN SQUARE (RMS) per person per meal
+# Check patient_rms_df for RMS Dataframe
+patient_rms_df = get_rms(patient_df)
