7d264e80f8955fe497107ad835a6e609c7fc5a5f
SujayGd <sujaygd14@gmail.com>
2019-11-04 01:26:02 -0700

    added get_meal_data() func to get two numpy arrays for meal data and class labels

--- helper.py
+++ helper.py
@@ -50,6 +50,34 @@ def get_data_frame():
 
     return patient_df
 
+def get_meal_array():
+    """
+    To convert meal data from CSVs in MealDataFolder to a numpy array and a class label array
+    Returns:
+        tuple of the form (numpy array of all data, numpy array of class labels)
+    """
+    k = ConfigParser()
+    k.read('config.ini')
+    directory = k['FILES']['meal_data_directory']
+    meal_data_np = []
+    class_labels_np = []
+
+    for meal_data_file in os.listdir(directory):
+        print("loading file - " + meal_data_file)
+        class_label = 0 if 'Nomeal' in meal_data_file else 1
+        meal_data = pd.read_csv(os.path.join(directory, meal_data_file), na_filter = False, header = None, sep = '\n')
+
+        for i,_ in enumerate(meal_data.iterrows()):
+            t = getFloatFromObjectForMealData(meal_data.loc[i])
+            print(t.shape)
+            meal_data_np.append(t)
+            class_labels_np.append(class_label)
+        
+    meal_data_np = np.array(meal_data_np)
+    class_labels_np = np.array(class_labels_np)
+
+    return (meal_data_np, class_labels_np)
+
 def convert_to_epoch(matlab_datenum):
     """converts shitty matlab time to Python datetime object
   
@@ -69,15 +97,28 @@ def getFloatFromObject(array):
     arrayStr = array.astype(np.str)
     newArray = []
     for item in arrayStr:
-        if item != '' : newArray.append(item)
+        if item == 'None': newArray.append(np.nan)
+        elif item != '' : newArray.append(item)
         elif item == 'NaN' : newArray.append(np.nan)
         elif item == 'Nan' : newArray.append(np.nan)
 
     return np.array(newArray).astype(np.float)
 
+def getFloatFromObjectForMealData(array):
+    arrayStr = list(array)[0].split(",")
+    newArray = []
+    for item in arrayStr:
+        if item == 'None': newArray.append(np.nan)
+        elif item != '' : newArray.append(item)
+        elif item == 'NaN' : newArray.append(np.nan)
+        elif item == 'Nan' : newArray.append(np.nan)
+
+    return np.array(newArray).astype(np.float)
 
 def get_patient_df(all_dfs):
     final = all_dfs[0]
     for i in range(1, len(all_dfs)):
         final = final.merge(all_dfs[i], how='left', on=['patient_number', 'meal_number'])
     return final
+
+get_meal_array()
