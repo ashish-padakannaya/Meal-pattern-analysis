5b79cd3461440c1b447409d600d7f58100f6f024
Vishnu Teja Yalakuntla <yvtheja@Vishnus-MacBook-Pro.local>
2019-11-10 13:53:30 -0700

    SVD

---
+++ enums/model.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class ModelType(Enum):
+    AMM = 1
+    SVM = 2
+    DT = 3
+    RF = 4
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from helper import get_meal_vectors
+from enums.model import ModelType
+from svm import trainSVM
 
 if __name__ == "__main__":
+
+    modelType = ModelType.SVM
     
     #get patient dataframe from CSV
     data, labels = get_meal_vectors()
-    print("data shape is ", data.shape)
 
-
-
+    if(modelType == ModelType.SVM):
+        trainSVM(data, labels)
